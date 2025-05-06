2a43700de6a494edb0d08dced16df47a8e209593
NachoBasilio <ignacio.n.basilio.b@gmail.com>
2025-05-05 22:36:02 -0300

    Final de clase

---
+++ Semana-1/main.js
@@ -0,0 +1,44 @@
+console.log("Hola mundo")
+
+//alert("Hola chicos")
+
+let nombre = "Carla"
+
+console.log(nombre)
+
+//alert(nombre)
+
+//let edad = prompt("Cuantos años tenes?")
+
+// edad = Number(edad)
+
+// console.log(isNaN(edad)) //number
+
+// edad = true
+
+// console.log(edad)
+//console.log(typeof edad) //boolean
+
+let quiereUnHelado = confirm("¿Quiere helado de vainilla?")
+
+console.log(quiereUnHelado)
+
+let apellido = prompt("¿Cual es tu apellido?")
+
+console.log(apellido)
+
+let valor1 = 0.1
+
+let valor2 = 0.2
+
+let valorRaro = valor1 + valor2
+
+console.log( valorRaro.toFixed(2) )
+
+const PI = 3.14
+
+const URL = "www.google.com"
+
+const nombreUsuarioActual = "Leonel"
+
+console.warn("Cumplir años es un error")
